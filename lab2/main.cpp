111d8d06fe6fb57884c07351806734da02f06d23
tehek <golden.bonnie.87@mail.ru>
2025-10-03 10:58:11 +0200

    useless

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -10,14 +10,13 @@ int main() {
 	short Achecker;
 	short i;
 	bool bitcheck;
-	unsigned char lowestbits = 0b11111111;
 	cout << "Введите A: ";
 	cin >> A;
-	Achecker = A & lowestbits;
+	Achecker = A;
 	cout << "Введите бит i (биты считаем слева направо): ";
 	cin >> i;
 	bitcheck = (Achecker >> 8 - i) & 1;
-	if (bitcheck) {
+	if (bitcheck==1) {
 		cout << "Битовый сдвиг A №1: " << (A = (A >> 1)) << endl;
 		cout << "Битовый сдвиг A №2: " << (A = (A >> 1)) << endl;
 		cout << "Битовый сдвиг A №3: " << (A = (A >> 1)) << endl;
@@ -27,7 +26,7 @@ int main() {
 		cout << "Введите число M: ";
 		cin >> M;
 		M -= 5;
-		(M != 0 ? (cout << (M < 0 ? "M меньше 5 на " : "M больше 5 на ") << M << endl) : (cout << "M равно 5" << endl));
+		(M != 0 ? (cout << (M < 0 ? "M меньше 5 на " : "M больше 5 на ") << (M<0? -M:M) << endl) : (cout << "M равно 5" << endl));
 	};
 	short N;
 	cout << "Введите N (1-7): ";
