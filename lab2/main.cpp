f987657467b0a42576168b0bfda45f20aaaee5bc
tehek <golden.bonnie.87@mail.ru>
2025-10-05 21:58:49 +0200

    bitset

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,7 @@
 // вариант 2
 
 #include <iostream>
+#include <bitset>
 
 using namespace std;
 
@@ -13,20 +14,21 @@ int main() {
 	cout << "Введите A: ";
 	cin >> A;
 	Achecker = A;
+	cout << bitset<8>(Achecker) << endl;
 	cout << "Введите бит i (биты считаем слева направо): ";
 	cin >> i;
 	bitcheck = (Achecker >> 8 - i) & 1;
-	if (bitcheck==1) {
-		cout << "Битовый сдвиг A №1: " << (A = (A >> 1)) << endl;
-		cout << "Битовый сдвиг A №2: " << (A = (A >> 1)) << endl;
-		cout << "Битовый сдвиг A №3: " << (A = (A >> 1)) << endl;
+	if (bitcheck == 1) {
+		cout << "Битовый сдвиг A №1: " << (A = (A >> 1)) << " = " << bitset<8>(A) << endl;
+		cout << "Битовый сдвиг A №2: " << (A = (A >> 1)) << " = " << bitset<8>(A) << endl;
+		cout << "Битовый сдвиг A №3: " << (A = (A >> 1)) << " = " << bitset<8>(A) << endl;
 	}
 	else {
 		short M;
 		cout << "Введите число M: ";
 		cin >> M;
 		M -= 5;
-		(M != 0 ? (cout << (M < 0 ? "M меньше 5 на " : "M больше 5 на ") << (M<0? -M:M) << endl) : (cout << "M равно 5" << endl));
+		(M != 0 ? (cout << (M < 0 ? "M меньше 5 на " : "M больше 5 на ") << (M < 0 ? -M : M) << endl) : (cout << "M равно 5" << endl));
 	};
 	short N;
 	cout << "Введите N (1-7): ";
