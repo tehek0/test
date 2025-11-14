d57c59d5252deb998d8e4928e0fcaaeb6d139f5c
tehek <golden.bonnie.87@mail.ru>
2025-11-14 15:36:36 +0200

    lab6 fixes

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -1,10 +1,11 @@
 // Вариант 2
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 int counter = 0;
 int* get_zeros(int** memory, int col, int row) {
-	int* arr = static_cast<int*>(calloc(row * col, 4));
+	int* arr = static_cast<int*>(calloc(row * col, sizeof(int)));
 	for (int i = 0; i < col; ++i) {
 		for (int j = 0; j < row; ++j) {
 			if (memory[j][i] == 0) {
@@ -17,7 +18,7 @@ int* get_zeros(int** memory, int col, int row) {
 
 }
 
-void read_memory(int** memory, int col, int row) {
+void print_memory(int** memory, int col, int row) {
 	for (int i = 0; i < col; ++i) {
 		for (int j = 0; j < row; ++j) {
 			cout << memory[j][i] << " ";
@@ -29,44 +30,63 @@ void read_memory(int** memory, int col, int row) {
 void item1() {
 	const char base_row = 2;
 	const char base_col = 2;
-	int base[base_row][base_col];
+	int** memory = static_cast<int**>(calloc(base_row, sizeof(int*)));
+	for (int i = 0; i < base_row; ++i) {
+		memory[i] = static_cast<int*>(calloc(base_col, sizeof(int)));
+	}
 
 	cout << "Введите A строк для добавления вверх: ";
-	cin >> base[0][0];
-	cout << "Введите B строк для добавления влево: ";
-	cin >> base[1][0];
-	cout << "Введите коэфициент C: ";
-	cin >> base[0][1];
-	cout << "Введите коэфициент Д: ";
-	cin >> base[1][1];
+	do {
+		cin >> memory[0][0];
+		if (memory[0][0] < 0)
+			cout << "Введите корректное значение: ";
+	} while (memory[0][0] < 0);
 
-	int row = (base_row + base[1][0]);
-	int col = (base_col + base[0][0]);
+	cout << "Введите B строк для добавления влево: ";
+	do {
+		cin >> memory[1][0];
+		if (memory[1][0] < 0)
+			cout << "Введите корректное значение: ";
+	} while (memory[1][0] < 0);
+
+	cout << "Введите коэфициент C: ";
+	cin >> memory[0][1];
+
+	cout << "Введите коэфициент Д: ";
+	cin >> memory[1][1];
+
+	int row = (base_row + memory[1][0]);
+	int col = (base_col + memory[0][0]);
+	size_t row_size = row * sizeof(int*);
+	size_t col_size = col * sizeof(int);
 	int indexes = row * col;
-	int** memory = (int**)calloc(row, sizeof(int*));
-	for (int i = 0; i < row; ++i) {
-		memory[i] = (int*)calloc(col, sizeof(int));
+	memory = static_cast<int**>(realloc(memory, row_size));
+	for (int i = base_row; i < row; ++i) {
+		memory[i] = static_cast<int*>(malloc(col_size));
 	}
+	memory[row - 1][col - 1] = memory[1][1];
+	memory[row - 1][col - 2] = memory[1][0];
+	memory[row - 2][col - 1] = memory[0][1];
+	memory[row - 2][col - 2] = memory[0][0];
+	
 
 	for (int i = 0; i < col; ++i) {
 		for (int j = 0; j < row; ++j) {
-
-			memory[j][i] = ((i * base[0][1]) + (j * base[1][1]));
-			if ((i == col - 1) && (j == row - 1)) {
-				memory[j - 1][i - 1] = base[0][0];
-				memory[j - 1][i] = base[0][1];
-				memory[j][i - 1] = base[1][0];
-				memory[j][i] = base[1][1];
+			if ((i >= col - 2) && (j == row - 2)) {
+				break;
 			}
+			memory[j][i] = ((i * memory[row - 2][col - 1]) + (j * memory[row - 1][col - 1]));
+			cerr << endl << ((i * memory[row - 2][col - 1]) + (j * memory[row - 1][col - 1]));
+			
 		}
 
 	}
 
+	cout << endl << "Исходная матрица: " << endl;
+	print_memory(memory, col, row);
+	cout << endl;
+
 	int new_col = 0;
-	int** new_memory = (int**)calloc(row, sizeof(int*));
-	for (int i = 0; i < row; ++i) {
-		new_memory[i] = (int*)calloc(col - counter, sizeof(int));
-	}
 	int* zero_array = get_zeros(memory, col, row);
 	for (int i = 0; i < col; ++i) {
 		for (int j = 0; j < row; ++j) {
@@ -80,25 +100,26 @@ void item1() {
 				--new_col;
 				break;
 			}
-			new_memory[j][new_col] = memory[j][i];
+			memory[j][new_col] = memory[j][i];
+			
 		}
 		++new_col;
 	}
-	cout << endl << "Исходная матрица: " << endl;
-	read_memory(memory, col, row);
-	cout << endl;
+	
+	
 
 	cout << "Новая матрица: " << endl;
-	read_memory(new_memory, new_col, row);
+	print_memory(memory, new_col, row);
 
-
-
-	for (int i = 0; i < row; i++) {
+	
+	for (int i = 0; i < row; ++i) {
+		memory[i] = static_cast<int*>(realloc(memory[i], col_size));
 		free(memory[i]);
 	}
-	free(zero_array);
+
 	free(memory);
-	free(new_memory);
+	free(zero_array);
+
 }
 
 void item2() {
@@ -132,4 +153,5 @@ int main() {
 	item1();
 	item2();
 	
+	exit(0);
 }
