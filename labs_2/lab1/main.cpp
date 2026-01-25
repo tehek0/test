6ae4394f1c5452f20e042b9488a9b0257737f642
tehek <golden.bonnie.87@mail.ru>
2026-01-25 15:42:05 +0200

    lab 2_1 final

--- labs_2/lab1/main.cpp
+++ labs_2/lab1/main.cpp
@@ -3,17 +3,63 @@
 
 using namespace std;
 
-const unsigned int _govIDlength = 8;
-const unsigned int _bodyIDminlength = 9;
-const unsigned int _bodyIDmaxlength = 12;
-
 class car {
+
+	const unsigned int _govIDlength = 6;
+	const unsigned int _bodyIDminlength = 9;
+	const unsigned int _bodyIDmaxlength = 12;
+
+	bool govIDformat(string& govid) {
+		if (govid.empty())
+			return false;
+		size_t govidlen = govid.length();
+		if (govidlen != _govIDlength)
+			return false;
+		for (int i = 0; i < govidlen; ++i) {
+			if ((i >= 1) && (i <= 3)) {
+				if (govid[i] >= '0' && govid[i] <= '9')
+					continue;
+				return false;
+			}
+			if ((isspace(govid[i]) == true) || (ispunct(govid[i]) == true) || (isalpha(govid[i]) == false))
+				return false;
+		}
+		return true;
+	}
+
+	bool bodyIDformat(string& bodyid) {
+		if (bodyid.empty())
+			return false;
+		size_t bodyidlen = bodyid.length();
+		if ((bodyidlen > _bodyIDmaxlength) || (bodyidlen < _bodyIDminlength))
+			return false;
+		for (int i = 0; i < bodyidlen; ++i) {
+			if ((isspace(bodyid[i]) == true) || (ispunct(bodyid[i]) == true) || (isascii(bodyid[i]) == false))
+				return false;
+		}
+		return true;
+	}
+
+	string _mark;
+	string _model;
+	string _govID;
+	string _bodyID;
+	unsigned int _mileage = 0;
+
+	
 
 public:
 
-	car() = default;
-
+	car() {
+		cerr << endl << "Запущен стандартный конструктор" << endl;
+	}
+	
 	car(const car& copy) {
+		if (&copy == nullptr) {
+			cout << endl << "Указан nullptr в конструкторе копирования" << endl;
+			exit(3);
+		}
+		cerr << endl << "Запущен конструктор копирования" << endl;
 		_mark = copy._mark;
 		_model = copy._model;
 		_govID = copy._govID;
@@ -22,6 +68,7 @@ public:
 	}
 
 	car(string mark, string model, string govid, string bodyid, unsigned int mileage) {
+		cerr << endl << "Запущен конструктор полного заполнения" << endl;
 		_mark = mark;
 		_model = model;
 		set_govID(govid);
@@ -30,7 +77,7 @@ public:
 	}
 
 	~car() {
-
+		cerr << endl << "Запущен деструктор" << endl;
 	}
 
 	string get_mark() {
@@ -48,27 +95,27 @@ public:
 	unsigned int get_mileage() {
 		return _mileage;
 	}
-	
+
 	void info() {
-	
+
 		cout << endl
-			 << "Марка: " << _mark << endl
-			 << "Модель: " << _model << endl
-			 << "Гос. номер: " << _govID << endl
-			 << "Номер кузова: " << _bodyID << endl
-			 << "Пробег: " << _mileage << " км." 
-			 << endl;
+			<< "Марка: " << _mark << endl
+			<< "Модель: " << _model << endl
+			<< "Гос. номер: " << _govID << endl
+			<< "Номер кузова: " << _bodyID << endl
+			<< "Пробег: " << _mileage << " км."
+			<< endl;
 	}
 
 	void set_govID(string id) {
 		try {
-			if (id.length() != _govIDlength) {
-				throw invalid_argument("[set_govID] Неподходящая длина гос. номера");
+			if (govIDformat(id) == false) {
+				throw invalid_argument("[set_govID] Неподходящий формат гос. номера");
 			}
-			
+
 			_govID = id;
 
-			}
+		}
 		catch (invalid_argument& error) {
 			cerr << error.what() << endl;
 			exit(1);
@@ -77,9 +124,8 @@ public:
 
 	void set_bodyID(string bodyid) {
 		try {
-			size_t bodyid_length = bodyid.length();
-			if ((bodyid_length > _bodyIDmaxlength) || (bodyid_length < _bodyIDminlength)) {
-				throw invalid_argument("[set_bodyID] Неподходящая длина номера кузова");
+			if (bodyIDformat(bodyid) == false) {
+				throw invalid_argument("[set_bodyID] Неподходящий формат номера кузова");
 			}
 
 			_bodyID = bodyid;
@@ -101,14 +147,7 @@ public:
 
 		_mileage -= X;
 	}
-
-private:
-	string _mark;
-	string _model;
-	string _govID;
-	string _bodyID;
-	unsigned int _mileage = 0;
-
+	
 };
 
 
@@ -118,15 +157,42 @@ private:
 int main() {
 
 	setlocale(LC_ALL, "Russian");
-	
-	car test2("Toyota", "Camry", "A123BC39", "123456789ABC", 10000);
+
+	car test1();
+	car test2("Toyota", "Camry", "A123BC", "123456789ABC", 10000);
 	car test3(test2);
 
-	test3.info();
+	cout << endl
+		<< test3.get_mark() << endl
+		<< test3.get_model() << endl
+		<< test3.get_govID() << endl
+		<< test3.get_bodyID() << endl
+		<< test3.get_mileage() << endl;
 	test2.lower_mileage(1500);
-	test2.set_govID("A333AA39");
+	test2.set_govID("A333AA");
 	test2.set_bodyID("12332332155");
 	test2.info();
 
-	car test1("Lada", "Priora", "D4561231", "1", 15000);
+
+		string model;
+		string mark;
+		string govid;
+		string bodyid;
+		unsigned int mileage;
+		 
+
+		cout << endl << "Введите модель: ";
+		cin >> model;
+		cout << endl << "Введите марку: ";
+		cin >> mark;
+		cout << endl << "Введите гос. номер (формат: A999AA): ";
+		cin >> govid;
+		cout << endl << "Введите номер кузова (9-12 символов): ";
+		cin >> bodyid;
+		cout << endl << "Введите километраж: ";
+		cin >> mileage;
+		cout << endl << endl << endl << endl << "=== === === Сборка === === ===" << endl;
+		car testfinal(mark, model, govid, bodyid, mileage);
+		testfinal.info();
+	
 }
